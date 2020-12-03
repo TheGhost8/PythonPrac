41623a9fb32d7f51e4b50170e37b7273e777e6af
Ivan <vantank36@gmial.com>
2020-12-03 16:20:03 +0300

    Done task 3 from 26.11.20. Tests for 3 task left.

--- 20201126_3/task_3.py
+++ 20201126_3/task_3.py
@@ -1,7 +1,21 @@
-from struct import *
-from random import uniform, randint
+import struct
+import zlib
+import sys
 
-f = open("o",)
+def readpack(fmt, fle):
+    return struct.unpack(fmt, fle.read(struct.calcsize(fmt)))
 
-for i in range(10):
-	f.write(pack(uniform(0.,1.), randint(0,10)))
+with open(sys.argv[1], "br") as f:
+    smth_useless = readpack("4c", f)
+    file_size = readpack("I", f)
+    smth_useless = readpack("8cI", f)
+    data_type = readpack("H", f)
+    channels_num = readpack("H", f)
+    sample_rate = readpack("I", f)
+    smth_useless = readpack("IH", f)
+    bits_in_sample = readpack("H", f)
+    smth_useless = readpack("4c", f)
+    data_size = readpack("I", f)
+    print('Size=', file_size[0], ', Type=', data_type[0],
+    	  ', Channels=', channels_num[0], ', Rate=', sample_rate[0],
+    	  ', Bits=', bits_in_sample[0], ', Data size=', data_size[0], sep='')
