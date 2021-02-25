c2c7f39a5ae075de6d0a04cc3280dcf8e09389d8
Ivan <vantank36@gmial.com>
2021-02-25 12:01:32 +0300

    Added buttons on rigth places.

--- 20210220_2/task_2.py
+++ 20210220_2/task_2.py
@@ -11,12 +11,16 @@ class Application(tk.Frame):
 		self.dir = tk.StringVar()
 		#self.date = 
 		#self.git = 
-		self.timeButton = tk.Button(self, text='Time')
+		self.dirButton = tk.Button(self, text='dir')
+		self.dateButton = tk.Button(self, text='date')
+		self.gitButton = tk.Button(self, text='git')
 		self.quitButton = tk.Button(self, text='Quit', command=self.quit)
 		self.Label = tk.Label(self, text=subprocess.run(["dir"], capture_output=True).stdout)
-		self.timeButton.grid()
-		self.quitButton.grid(row=0, column=1)
-		self.Label.grid(columnspan=2)
+		self.dirButton.grid(row=0, column=0)
+		self.dateButton.grid(row=0, column=1)
+		self.gitButton.grid(row=0, column=2)
+		self.quitButton.grid(row=1, column=1)
+		self.Label.grid(row=2, column=1)
 
 app = Application()
 app.master.title('Display commands application')
