9d3e1d876caa5c8d38b93e83e9c1f850178f3054
Ivan <vantank36@gmial.com>
2021-03-01 11:58:36 +0300

    Added next item button.

--- 20210301_1/task_1.py
+++ 20210301_1/task_1.py
@@ -11,12 +11,17 @@ class Application(tk.Frame):
 		"""Handler for exit button."""
 		pass
 
+	def nextItemButton(self):
+		"""Handler for next item button."""
+		pass
 
 	def createWidgets(self):
+		self.nextItemButton = tk.Button(self, text='Next Item', command=self.nextItemButton)
 		self.quitButton = tk.Button(self, text='Quit', command=self.quitButton)
 		self.Label = tk.Label(self, text='<MenuItem>')
 		self.Label.grid(row=0)
-		self.quitButton.grid(row=1)
+		self.nextItemButton.grid(row=1, column=0)
+		self.quitButton.grid(row=1, column=1)
 		pass
 
 app = Application()
